a1f4ab62222251537073cd3d1f6f56f649320441
Roop kishore <wizsrk@gmail.com>
2019-10-10 15:40:15 -0400

    Async in Vertx

--- src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -10,7 +10,21 @@ public class HelloWorldEmbedded {
 
   public static void main(String[] args) {
     // Create an HTTP server which simply returns "Hello World!" to each request.
-    Vertx.vertx().createHttpServer().requestHandler(req -> req.response().end("Hello World!")).listen(8080);
+    Vertx.vertx().createHttpServer().requestHandler(req -> {
+   
+      Vertx.vertx().executeBlocking(future ->{
+        
+        try {
+          Thread.sleep(1500);
+        } catch (Exception e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
+        future.complete();
+      }, res -> {
+        req.response().end("Hello Improved Delay!");
+      });
+    }).listen(8080);
   }
 
 }
