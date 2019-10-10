ee101efd2efeb029517cb774d0fc6a711de7fed4
Roop kishore <wizsrk@gmail.com>
2019-10-10 16:09:31 -0400

    Unordered changes

--- src/main/java/io/vertx/example/HelloWorldEmbedded.java
+++ src/main/java/io/vertx/example/HelloWorldEmbedded.java
@@ -12,8 +12,8 @@ public class HelloWorldEmbedded {
   public static void main(String[] args) {
     // Create an HTTP server which simply returns "Hello World!" to each request.
     VertxOptions options = new VertxOptions();
-    options.setEventLoopPoolSize(100);
-    options.setWorkerPoolSize(1000);
+    //options.setEventLoopPoolSize(100);
+    //options.setWorkerPoolSize(1000);
     Vertx ver = Vertx.vertx(options);
     ver.createHttpServer().requestHandler(req -> {
    
